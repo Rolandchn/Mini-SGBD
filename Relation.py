fde52b41e29bcaf17941c961988204d83cc7c42e
Lylichen07 <feedpoipoi@gmail.com>
2024-10-16 15:06:44 +0200

    code writeRecordToBuffer() and comments

--- Relation.py
+++ Relation.py
@@ -15,12 +15,27 @@ class Relation:
 
     def writeRecordToBuffer(record: Record, buff: Buffer, pos: int) -> int:
         buff.byteBuffer.set_position(pos)
+
+        # template
         for stuff in record.values:
             try:
-                buff.byteBuffer.put_int(float(stuff))
+                # check
+                int(stuff)
 
-                # cas où stuff est un int ?
+                # cas où c'est un float
+                if "." in stuff:
+                    buff.byteBuffer.put_float(float(stuff))
 
-            except (TypeError, ValueError):
+                # cas où c'est un int
+                else:
+                    buff.byteBuffer.put_int(int(stuff))
+
+            # cas où c'est un pur string
+            except (ValueError):
                 # doit écrire tout le string, caractère par caractère 
                 buff.byteBuffer.put_char(stuff)
+        # template end
+
+
+    def readFromBuffer(record: Record, buff: Buffer, pos: int) -> int:
+        ...
