d4097dccb5f2fb794a6f1663b5c021e813dc013b
Lylichen07 <feedpoipoi@gmail.com>
2024-10-16 14:51:27 +0200

    add writeRecordToBuffer() and comments

--- Record.py
+++ Record.py
@@ -4,4 +4,4 @@ class Record:
     def __init__(self, values:tuple):
         self.values = list(values)
 
-    
+        # ou self.value1, self.value2 ?
--- Relation.py
+++ Relation.py
@@ -1,6 +1,9 @@
 
 import Column
 
+from Buffer import Buffer
+from Record import Record
+
 
 class Relation:
     def __init__(self, name: str, num_column: int):
@@ -9,4 +12,15 @@ class Relation:
         
         self.columns: tuple[Column.ColumnType] = tuple()
     
-    
+
+    def writeRecordToBuffer(record: Record, buff: Buffer, pos: int) -> int:
+        buff.byteBuffer.set_position(pos)
+        for stuff in record.values:
+            try:
+                buff.byteBuffer.put_int(float(stuff))
+
+                # cas où stuff est un int ?
+
+            except (TypeError, ValueError):
+                # doit écrire tout le string, caractère par caractère 
+                buff.byteBuffer.put_char(stuff)
