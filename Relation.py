e3693e48846c49ef3dcdc890e6cd3d752b034611
Ghilas Tidjet <ghilastidjet7@gmail.com>
2024-10-17 16:00:04 +0200

    correction

--- Relation.py
+++ Relation.py
@@ -15,7 +15,7 @@ class Relation:
     
 
     def writeRecordToBuffer(self, record: Record, buff: Buffer, pos: int) -> int:
-        buff.byteBuffer.set_position(pos)
+        buff.set_position(pos)
 
         for stuff in record.values:
             try:
@@ -23,25 +23,25 @@ class Relation:
 
                 # cas où c'est un float
                 if "." in stuff:
-                    buff.byteBuffer.put_float(float(stuff))
+                    buff.put_float(float(stuff))
                     self.columns += Column.ColumnType.REAL
 
                 # cas où c'est un int
                 else:
-                    buff.byteBuffer.put_int(int(stuff))
+                    buff.put_int(int(stuff))
                     self.columns += Column.ColumnType.INT
 
             # cas où c'est un pur string
             except (ValueError):
                 # doit écrire tout le string, caractère par caractère 
-                buff.byteBuffer.put_char(stuff)
+                buff.put_char(stuff)
                 self.columns += Column.ColumnType.CHAR
                 
                 # cas où c'est un pur string variable ?
 
 
     def readFromBuffer(self, record: Record, buff: Buffer, pos: int) -> int:
-        buff.byteBuffer.set_position(pos)
+        buff.set_position(pos)
 
         for column in self.columns:
             ...
