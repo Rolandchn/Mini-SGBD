970e9a128deaec7c0eea1c87889abff297f788fe
Lylichen07 <feedpoipoi@gmail.com>
2024-10-24 16:49:13 +0200

    implement writeRecordToBuffer

--- Relation.py
+++ Relation.py
@@ -33,8 +33,49 @@ class Relation:
                         buff.put_char(value[i])
 
         else:
-            ...
-        
+            init_pos = buff.__pos
+            pos_buffer = buff.__pos + 4 * (self.nb_column + 1)
+            
+            buff.put_int(pos_buffer)
+            init_pos = buff.__pos
+
+            for index, value in enumerate(record.values):
+                buff.set_position(pos_buffer)
+
+                data = self.columns[index]
+
+                if isinstance(data, Column.Number):
+                    if type(data.value) == float:
+                        buff.put_float(float(value))
+                    
+                    else:
+                        buff.put_int(int(value))
+
+                    buff.set_position(init_pos)
+                    buff.put_int(pos_buffer + 4)
+
+                    pos_buffer = buff.__pos + 4
+
+                
+                elif isinstance(data, Column.Char):
+                    for i in range(data.size):
+                        buff.put_char(value[i])
+
+                    buff.set_position(init_pos)
+                    buff.put_int(pos_buffer + data.size)
+
+                    pos_buffer = buff.__pos
+                
+                else:
+                    for i in range(data.size):
+                        buff.put_char(value[i])
+
+                    buff.set_position(init_pos)
+                    buff.put_int(pos_buffer + data.size)
+
+                    pos_buffer = buff.__pos
+
+
     @staticmethod
     def has_varchar(columns):
         for stuff in columns:
