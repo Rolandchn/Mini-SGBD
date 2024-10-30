1f64b31531afe28b1a870b8043029ca21c56835a
Lylichen07 <feedpoipoi@gmail.com>
2024-10-30 15:24:24 +0100

    refactor method

--- Relation.py
+++ Relation.py
@@ -56,66 +56,38 @@ class Relation:
                 buff.put_char(char)
 
 
-    def readFromBuffer(self, record, buff, pos) -> int:
-        buff.set_position(pos)
+    def readFromBuffer(self, record: Record, buff: Buffer, pos: int) -> int:
+        has_varchar = self.has_varchar(self.columns)
+
+        if has_varchar:
+            self.put_offset_to_buffer(buff.__pos + 4 * (self.nb_column + 1), pos, buff)
+            adress_pos = pos + 4
         
-        if not self.has_varchar(self.columns):
-            for index, value in enumerate(record.values):
-                data = self.columns[index]
-
-                if isinstance(data, Column.Number):
-                    if type(data.value) == float:
-                        buff.read_float(float(value))
-                    
-                    else:
-                        buff.read_int(int(value))
-                
-                elif isinstance(data, Column.Char):
-                    for i in range(data.size):
-                        buff.read_char(value[i])
-
         else:
-            value_pos = buff.read_int()
+            buff.set_position(pos)
+
+        # record = single row of values 
+        for value_info in self.columns:
+            value = self.read_value_from_buffer(value_info, buff)
+
+            if has_varchar:
+                self.put_offset_to_buffer(buff.__pos, adress_pos, buff)
+                adress_pos += 4
+        
+        return adress_pos - pos
+    
+    
+    def read_value_from_buffer(value_info: Column.ColumnInfo, buff: Buffer) -> int | float | str:
+        if isinstance(value_info.type, Column.Number):
+            if type(value) == float:
+                buff.read_float(float(value))
             
-            buff.put_int(value_pos)
-            adress_pos = buff.__pos
+            else:
+                buff.read_int(int(value))
 
-            for index, value in enumerate(record.values):
-                buff.set_position(value_pos)
-
-                data = self.columns[index]
-
-                if isinstance(data, Column.Number):
-                    if type(data.value) == float:
-                        buff.read_float(float(value))
-                    
-                    else:
-                        buff.read_int(int(value))
-
-                    buff.set_position(adress_pos)
-                    buff.read_int(value_pos + 4)
-
-                    value_pos = buff.__pos + 4
-
-                
-                elif isinstance(data, Column.Char):
-                    for i in range(data.size):
-                        buff.put_char(value[i])
-
-                    buff.set_position(adress_pos)
-                    buff.read_int(value_pos + data.size)
-
-                    value_pos = buff.__pos
-                
-                else:
-                    for i in range(data.size):
-                        buff.read_char(value[i])
-
-                    buff.set_position(adress_pos)
-                    buff.read_int(value_pos + data.size)
-
-                    value_pos = buff.__pos
-
+        elif isinstance(value_info.type, Column.Char):
+            for char in value:
+                buff.read_char(char)
 
     @staticmethod
     def has_varchar(columns):
@@ -124,9 +96,3 @@ class Relation:
                 return True
             
         return False
-
-    def readFromBuffer(self, record: Record, buff: Buffer, pos: int) -> int:
-        buff.set_position(pos)
-
-        for column in self.columns:
-            ...
