f7baea1aaddedd25697c77997352d1152938bf7a
Ghilas Tidjet <ghilastidjet7@gmail.com>
2024-12-16 07:24:54 +0100

    c

--- src/core/SGBD.py
+++ src/core/SGBD.py
@@ -1,5 +1,5 @@
 from typing import Optional, List
-from src.core.DBManager import DBManager
+from DBManager import DBManager
 from DiskManager import DiskManager
 from BufferManager import BufferManager
 from DBconfig import DBconfig
